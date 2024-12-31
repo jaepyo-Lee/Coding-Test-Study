19a62d7c9ddffc276803762279e085e121bba177
dy102 <132787602+dy102@users.noreply.github.com>
2024-12-31 20:47:16 +0900

    Update [3차] 압축.java

--- 김도연/[3차] 압축.java
+++ 김도연/[3차] 압축.java
@@ -1,29 +1,32 @@
 import java.util.*;
 class Solution {
- Map<String, Integer> map = new HashMap<>();
+   Map<String, Integer> map = new HashMap<>();
 
     public int[] solution(String msg) {
         List<Integer> arr = new ArrayList<>();
 
         int num = 26;
+        int tmp = 0;
         for (int i = 0; i < msg.length(); i++) {
             int a = a(msg, i, i);
             if (a - i == 0) {
+                tmp = i;
                 arr.add(msg.charAt(i) - 'A' + 1);
             } else {
                 arr.add(map.get(msg.substring(i, a + 1)));
-                i += (a - i);
+                tmp = i;
+                i = a;
             }
             if (msg.length() - 1 != i) {
-                map.put(msg.substring(i, a + 2), ++num);
-           //     System.out.println(msg.substring(i, a + 2));
+                map.put(msg.substring(tmp, a + 2), ++num);
+                System.out.println(msg.substring(tmp, a + 2));
             }
         }
 
         int[] answer = new int[arr.size()];
         for (int i = 0; i < arr.size(); i++) {
             answer[i] = arr.get(i);
-          //  System.out.println(answer[i]);
+            System.out.println(answer[i]);
         }
         return answer;
     }
@@ -36,3 +39,4 @@ class Solution {
         }
     }
 }
+
