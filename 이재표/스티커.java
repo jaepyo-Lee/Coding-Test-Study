03cfceb2c2ba3fa189266fe02860697ef5c2f8a6
jaepyo-Lee <jaepyo99@naver.com>
2025-01-05 22:34:29 +0900

    이재표/BJ/스티커

---
+++ 이재표/스티커.java
@@ -0,0 +1,29 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            int n = Integer.parseInt(br.readLine());
+            int[][] sticker = new int[2][n];
+            for (int j = 0; j < 2; j++) {
+                sticker[j] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            }
+            int[][]dp=new int[2][n];
+            dp[0][1]=sticker[0][1]+sticker[1][0];
+            dp[1][1]=sticker[1][1]+sticker[0][0];
+            dp[0][0]=sticker[0][0];
+            dp[1][0]=sticker[1][0];
+            for(int j=2;j<n;j++){
+                dp[0][j]=Math.max(dp[1][j-1],dp[1][j-2])+sticker[0][j];
+                dp[1][j]=Math.max(dp[0][j-1],dp[0][j-2])+sticker[1][j];
+            }
+
+            System.out.println(Math.max(dp[0][n-1],dp[1][n-1]));
+
+        }
+    }
+}
