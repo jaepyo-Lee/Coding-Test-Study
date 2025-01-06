5612e3b70943e2deff138b3183973064804e37b0
탁세하 <takseha98@gmail.com>
2025-01-06 19:09:13 +0900

    탁세하/BJ/바이러스

---
+++ 탁세하/BJ/바이러스.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+/*
+ * 입력값이 dfs 입력값 느낌
+ * : 각 입력 값을 pair<int, int>로 받고, first와 second를 비교하며 cnt++
+ */
+
+int n, m;
+int cnt;
+bool vis[101];
+vector<pair<int, int>> vec;
+
+// 양방향 간선이기 때문에 start == first인 경우와 start == second인 경우 둘 다 생각!
+void dfs(int start) {
+    for (int i = 0; i < m; i++) {
+        if (vec[i].first == start && !vis[vec[i].second]) {
+            cnt++;
+            vis[vec[i].second] = true;
+            dfs(vec[i].second);
+        } else if (vec[i].second == start && !vis[vec[i].first]) {
+            cnt++;
+            vis[vec[i].first] = true;
+            dfs(vec[i].first);
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        int first, second;
+        cin >> first >> second;
+        vec.push_back({first, second});
+    }
+
+    vis[1] = true;
+    dfs(1);
+
+    cout << cnt;
+}
