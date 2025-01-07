1d360ae93ede91536102e37e245317354df941f6
이연걸 <dusrjf14@gmail.com>
2025-01-07 21:17:59 +0900

    Update 숨바꼭질3.py

--- 이연걸/숨바꼭질3.py
+++ 이연걸/숨바꼭질3.py
@@ -3,18 +3,22 @@ from collections import deque
 n, k = map(int, input().split())
 
 road = [0] * (100001)
-road[n], road[k] = -1, -10 # 수빈이와 동생을 직선 상에 둔다.
 
-# 수빈이의 이동을 표현하면 [1, -1, 0]
 q = deque()
 q.append(n)
+road[n] = 1
 while q:
     cur = q.popleft()
-    for d in [1, -1, 0]: # x+1, x-1, 2x
-        if d == 0: cur *= 2
-        else: cur += d
+    if cur == k:
+        break
+    for nxt in [cur * 2, cur - 1, cur + 1]:
 
-        if 0 <= cur <= 100000 and road[cur] >= 0:
+        if 0 <= nxt <= 100000:
+            if road[nxt] == 0:
+                if nxt == cur * 2:
+                    road[nxt] = road[cur]
+                else:
+                    road[nxt] = road[cur] + 1
+                q.append(nxt)
 
-
-# 진행중..
+print(road[k] - 1)
