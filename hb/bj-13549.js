fcd547d2d2b9ce395797dfaae23cbae14c51e9cd
Hur Hyeon Bin (Max) <160996936+hyeonbinHur@users.noreply.github.com>
2025-01-08 18:20:37 +0900

    2025/01/08 허현빈

--- hb/bj-13549.js
+++ hb/bj-13549.js
@@ -1,18 +1,22 @@
 // @ts-nocheck
 const input = require("fs")
-  .readFileSync("./dev/stdin.txt")
+  .readFileSync(0)
   .toString()
   .trim()
   .split("\n")[0]
   .split(" ")
   .map((e) => Number(e));
 const max = Math.max(input[0], input[1]);
-const visit = Array(100000).fill(0);
+const visit = Array(100001).fill(0);
+
+// 5, 9
+// 5-> 10 -> 9
+
 visit[input[0]] = 1;
 const ans = () => {
   let q = [input[0]];
   while (q.length) {
-    const cur = q.shift();
+    const cur = q.shift(); // shift == bfs, pop == dfs
     for (let i = 0; i < 3; i++) {
       let val = 0;
       if (i === 0) {
