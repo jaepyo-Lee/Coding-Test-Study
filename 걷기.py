54ee1e139b7aa82d7665f9eea94721968e8f3c50
KelseyKwon <nykwon7777@gmail.com>
2025-01-14 21:55:08 +0900

    권나연/GREEDY_BS_DP

--- 걷기.py
+++ 걷기.py
@@ -1,9 +1,10 @@
 def min_time():
-    diagonal = min(x, y)
-    left_straight = max(x, y) - diagonal
     if 2*w < s:
         return (x+y)*w
     else:
-        return (diagonal*s + left_straight*w)
+        if ((x+y) % 2 == 0) :
+            return (s * min(x, y)) + (min(w, s) * abs(x-y))
+        else:
+            return (s * min(x, y)) + (min(w, s) * (abs(x-y)-1)) + w
 x, y, w, s = map(int, input().split())
 print(min_time())
