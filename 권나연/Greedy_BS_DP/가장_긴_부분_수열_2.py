a75e2aaf47055a139a1a3957a7583c56f011aed7
KelseyKwon <nykwon7777@gmail.com>
2025-01-15 20:56:30 +0900

    "권나연/Greedy_BS_DP/가장긴수열"

---
+++ 권나연/Greedy_BS_DP/가장_긴_부분_수열_2.py
@@ -0,0 +1,8 @@
+N = int(input())
+sequence = list(map(int,input().split()))
+D = [1] * N
+for i in range(1, N):
+    for j in range(i):
+        if sequence[i] > sequence[j]:
+            D[i] = max(D[i], D[j] + 1)
+print(max(D))
