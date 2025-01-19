d873881c73cb7a6bd7bb61599f639df4778d5b36
mipangg <morganjeong119@gmail.com>
2025-01-20 00:59:25 +0900

    정미광/BJ/1459, 16401, 1699, 15486

--- 정미광/BJ/1699.java
+++ 정미광/BJ/1699.java
@@ -1,4 +1,5 @@
 
+import static java.lang.Math.*;
 import java.util.*;
 
 public class Main {
@@ -6,49 +7,59 @@ public class Main {
     public static void main(String[] args) {
         solution();
 //        Solution s = new Solution();
-//        System.out.println(s.solution(3, new int[][]{{1, 1, 0}, {1, 1, 1}, {0, 1, 1}}));
+//        System.out.println(s.solution(6, new int[]{7, 10}));
     }
 
     static void solution() {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int[] dp = new int[100001];
-
         //1 = 1
         //2 = 1 + 1
         //3 = 1 + 1 + 1
         //4 = 2
-        //5 = 2 + 1 = dp[4] + dp[1]
-        //6 = 2 + 1 + 1 = dp[4] + dp[2]
-        //7 = 2 + 1 + 1 + 1 = dp[4] + dp[3];
-        //8 = 2 + 2 = dp[4] + dp[4]
+        //5 = 2 + 1
+        //6 = 2 + 1 + 1
+        //7 = 2 + 1 + 1 + 1
+        //8 = 2 + 2
         //9 = 3
-        //10 = 3 + 1 = dp[9] + dp[1]
-        //11 = 3 + 1 + 1 = dp[9] + dp[2]
-        //12 = 3 + 1 + 1 + 1 = dp[9] + dp[3]
-        //13 = 3 + 2 = dp[9] + dp[4]
-        //14 = 3 + 2 + 1 = dp[9] + dp[5]
-        //15 = 3 + 2 + 1 + 1 = dp[9] + dp[6]
+        //10 = 3 + 1
+        //11 = 3 + 1 + 1
+        //12 = 3 + 1 + 1 + 1
+        //13 = 3 + 2
+        //14 = 3 + 2 + 1
+        //15 = 3 + 2 + 1 + 1
         //16 = 4
+        //17 = 4 + 1
+        //18 = 4 + 1 + 1
+        //19 = 4 + 1 + 1 + 1
+        //20 = 4 + 2
+        //21 = 4 + 2 + 1
+        int n = sc.nextInt();
+        int[] dp = new int[n + 1];
+
+        if (n < 4) {
+            System.out.print(n);
+            return;
+        }
 
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 3;
-        if (n < 4) {
-            System.out.print(dp[n]);
-            return;
-        }
-
-        for (int i = 4, j = 0; i <= n; i++, j++) {
-            int s = (int) Math.sqrt(i);
-            if (i == s * s) {
+        for (int i = 4, j = 1; i <= n; i++, j++) {
+            int elem = (int)sqrt(i);
+            if (i == elem * elem) {
                 dp[i] = 1;
                 j = 0;
                 continue;
             }
-            dp[i] = dp[j] + 1;
+
+            dp[i] = dp[j] + 1; //41: 6 + 2 + 1
+
+            for (int k = 1; k <= (i / 2); k++) { //41: 5 + 4
+                dp[i] = min(dp[i], dp[k] + dp[i - k]);
+            }
         }
+
         System.out.print(dp[n]);
 
         sc.close();
