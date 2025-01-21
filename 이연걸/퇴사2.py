e6205d71a46aba3fc8027d89f064c09323461170
이연걸 <dusrjf14@gmail.com>
2025-01-21 09:29:53 +0900

    Update 퇴사2.py

--- 이연걸/퇴사2.py
+++ 이연걸/퇴사2.py
@@ -1,25 +1,16 @@
 n = int(input())
+work = [[0, 0]]
+for _ in range(n): work.append(list(map(int, input().split())))
 
-table = []
-for _ in range(n):
-    table.append(list(map(int, input().split())))
-
-# dp[i][j] = i일차에
+dp = [0 for _ in range(n + 1)]
+for i in range(1, n + 1):
+    dp[i] = max(dp[i], dp[i-1])
+    fin_date = i + work[i][0] - 1
+    if fin_date <= n:
+        dp[fin_date] = max(dp[fin_date], dp[i-1] + work[i][1])
+print(dp[n])
 '''
-dp[1][0] : 1일차에 1일차꺼를 집을 수 있을 때 최대 값
-dp[1][1] : 1일차에 
-
-특정 상담을 하기 위해서는 잔여 일수가 충분해야 하고, 현재 상담중인 사람이 없어야함.
-
-dp[1] = 0
-dp[2] = 0
-dp[3] = 10
-dp[4] = 10 + 10
-dp[5] = 10 + 20
-dp[6] = 10 + 20 + 15
-dp[7] = 10 + 20 + 15
+0    1   2   3   4   5   6   7
+0    3   5   1   1   2   4   2
+    10  20  10  20  15  40  200
 '''
-
-
-
-print(table)
