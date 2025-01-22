2f73bdf1d9375cebf5ed179059990c2764035b31
이연걸 <dusrjf14@gmail.com>
2025-01-22 10:38:10 +0900

    Update 타임머신.py

--- 이연걸/타임머신.py
+++ 이연걸/타임머신.py
@@ -4,28 +4,24 @@ for _ in range(m):
     a, b, c = map(int, input().split())
     graph.append([a, b, c])
 
+dist = [1e12] * (n+1)
 def bmfd(st):
-    dist = [1e9] * (n+1)
     dist[st] = 0
     
     for i in range(n): # n개의 노드 모두 확인
         for j in range(m): # m개의 간선 모두 확인
             cur, nxt, wei = graph[j][0], graph[j][1], graph[j][2]
 
-            if dist[cur] != 1e9 and dist[cur] + wei < dist[nxt]:
+            if dist[cur] != 1e12 and dist[cur] + wei < dist[nxt]:
                 dist[nxt] = dist[cur] + wei
-    
-    for j in range(m):
-        cur, nxt, wei = graph[j][0], graph[j][1], graph[j][2]
-        if dist[cur] + wei < dist[nxt]:
-            return [] # 음의 순환 존재
-    return dist
+                if i == n - 1:
+                    return True
+    return False
 
-dist = bmfd(1)
-if len(dist) == 0: print(-1)
+if bmfd(1): print(-1)
 else:
     for i in range(2, n + 1):
-        if dist[i] == 1e9:
+        if dist[i] == 1e12:
             print(-1)
         else:
             print(dist[i])
