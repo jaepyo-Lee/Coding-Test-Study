2e978ccbdf04321c6d2fac46f87d30f95c7a2f4c
이연걸 <dusrjf14@gmail.com>
2025-02-04 19:47:33 +0900

    Update 평범한배낭.py

--- 이연걸/평범한배낭.py
+++ 이연걸/평범한배낭.py
@@ -2,13 +2,12 @@ n, k = map(int, input().split())
 stuff_list = [list(map(int, input().split())) for _ in range(n)]
 dp = [0] * (k+1)
 
+# for wei, val in stuff_list:
+#     for i in range(wei, k+1):
+#         dp[i] = max(val + dp[i-wei], dp[i])
+
 for wei, val in stuff_list:
-    for i in range(wei, k+1):
-        # if wei > i:
-        #     continue #지금 들고 있는 물건이 무게 i보다 크면 못넣음\
-        # elif wei == i:
-        #     dp[i] = max(val, dp[i])
-        # else:
+    for i in range(k, wei-1, -1):
         dp[i] = max(val + dp[i-wei], dp[i])
-    # print(dp)
+
 print(dp[k])
